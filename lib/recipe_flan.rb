ca43627cfa6831eb09740f6935a4116c35d34581
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-12 19:41:06 +0000

    modified cli.rb

--- lib/recipe_flan.rb
+++ lib/recipe_flan.rb
@@ -1,6 +1,2 @@
 require "recipe_flan/version"
 
-module RecipeFlan
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/recipe_flan/cli.rb
@@ -0,0 +1,5 @@
+class RecipeFlan::CLI
+  def call 
+    puts
+  end 
+end 
