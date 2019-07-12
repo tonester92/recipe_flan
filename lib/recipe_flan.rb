45f1b5c13051c1b258c0e325167f39d00eee9a58
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-12 21:41:57 +0000

    modified recipe.rb

--- lib/recipe_flan.rb
+++ lib/recipe_flan.rb
@@ -1,3 +1,5 @@
+require_relative
+
 require_relative "./recipe_flan/version"
 require_relative './recipe_flan/recipe'
 require_relative './recipe_flan/cli'
--- lib/recipe_flan/recipe.rb
+++ lib/recipe_flan/recipe.rb
@@ -2,23 +2,28 @@ class RecipeFlan::Recipe
 attr_accessor :name, :ingredients, :directions, :url 
 
   def self.today
-    #puts <<-DOC.gsub /^\s*/, '' #removes the whitespace
-    #1. Food Network Flan Created By Tyler Florence 
-    #2. All Recipe Flan Created By ASOTO
-    #DOC
+    #scrape Food Network and All Recipe and then return deals based on that data
+    #extract the properties 
+    #instantiate a recipe
     
-    recipe_1 = self.new
-    recipe_1.name = "Food Network Flan Created By Tyler Florence"
-    recipe_1.ingredients = "milk,sugar"
-    recipe_1.directions = "turn on oven"
-    recipe_1.url = "https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016"
+    self.scrape_recipes
+  end
+  
+  def self.scrape_recipes  
+    recipe = []
     
-    recipe_2 = self.new
-    recipe_2.name = "All Recipe Flan Created By ASOTO"
-    recipe_2.ingredients = "milk,sugar"
-    recipe_2.directions = "turn on oven"
-    recipe_2.url = "https://www.allrecipes.com/recipe/20979/spanish-flan"
+    #recipe_1 = self.new
+    #recipe_1.name = "Food Network Flan Created By Tyler Florence"
+    #recipe_1.ingredients = "milk,sugar"
+    #recipe_1.directions = "turn on oven"
+    #recipe_1.url = "https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016"
     
-    [recipe_1 , recipe_2]
+    #recipe_2 = self.new
+    #recipe_2.name = "All Recipe Flan Created By ASOTO"
+    #recipe_2.ingredients = "milk,sugar"
+    #recipe_2.directions = "turn on oven"
+    #recipe_2.url = "https://www.allrecipes.com/recipe/20979/spanish-flan"
+    
+    #[recipe_1 , recipe_2]
   end 
 end
