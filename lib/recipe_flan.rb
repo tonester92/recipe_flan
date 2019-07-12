ebb05bea384759437c82d4f7921ea8ed936c95e2
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-12 20:11:31 +0000

    modified recipe.rb

--- lib/recipe_flan.rb
+++ lib/recipe_flan.rb
@@ -1,2 +1,3 @@
 require_relative "./recipe_flan/version"
 require_relative './recipe_flan/cli'
+require_relative './recipe_flan/recipe'
--- lib/recipe_flan/recipe.rb
+++ lib/recipe_flan/recipe.rb
@@ -1,5 +1,40 @@
 class RecipeFlan::RecipeFlan
+  attr_accessor :name , :ingredients , :directions , :url
   
   def self.all 
+   self.scrape_recipe
   end
-end   
+
+  def self.scrape_recipe
+    recipes = []
+   
+    recipes << self.scrape_foodnetwork
+    recipes << self.scrape_allrecipes
+    
+    recipes
+  end
+
+  def self.scrape_foodnetwork
+    doc = Nokogiri::HTML(open(“https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016”))
+     
+    recipe_1 = self.new
+    recipe_1.name = 
+    recipe_1.ingredients =
+    recipe_1.directions =
+    recipe_1.url =
+    
+    recipe_1
+  end 
+
+  def self.scrape_allrecipes
+    doc = Nokogiri::HTML(open(“https://www.allrecipes.com/recipe/20979/spanish-flan/”))
+    
+    recipe_2 = self.new
+    recipe_2.name = 
+    recipe_2.ingredients =
+    recipe_2.directions =
+    recipe_2.url =
+
+    recipe_2 
+  end 
+end
