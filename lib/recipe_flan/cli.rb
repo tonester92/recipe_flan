43440bb79a1591f0dac1f1fac9f7d8ed805ddd06
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-12 20:58:43 +0000

    modified cli.rb

--- lib/recipe_flan/cli.rb
+++ lib/recipe_flan/cli.rb
@@ -15,15 +15,19 @@ class RecipeFlan::CLI
   end
   
   def direction
-    puts "Enter the number for the recipe you want or type exit to enter:"
     input = nil 
     while input != "exit"
-      
+       puts "Enter the number for the recipe you want or type exit to enter:"
+      input = gets.strip.downcase
       case input
       when "1"
         puts "More info on recipe 1..."
       when "2"
         puts "More info on recipe 2..."
+      when "list"
+        list_recipes
+      else
+        puts "Not sure what you want, type or exit."
       end
     end
   end 
