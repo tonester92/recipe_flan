bf9e34171c683f7e83207bb0e2fd057ff8abd0d7
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-12 20:48:38 +0000

    modified cli.rb

--- lib/recipe_flan/cli.rb
+++ lib/recipe_flan/cli.rb
@@ -3,6 +3,8 @@ class RecipeFlan::CLI
   def call 
     puts "Hello There, Here Is Two Flan Dessert Recipes! Which One Is Better for You?"
     list_recipes
+    direction
+    goodbye
   end  
  
   def list_recipes
@@ -10,5 +12,23 @@ class RecipeFlan::CLI
     1. Food Network Flan Created By Tyler Florence 
     2. All Recipe Flan Created By ASOTO
     DOC
+  end
+  
+  def direction
+    puts "Enter the number for the recipe you want or type exit to enter:"
+    input = nil 
+    while input != "exit"
+      
+      case input
+      when "1"
+        puts "More info on recipe 1..."
+      when "2"
+        puts "More info on recipe 2..."
+      end
+    end
+  end 
+  
+  def goodbye
+    puts "Which One is Better for You?"
   end
 end 
