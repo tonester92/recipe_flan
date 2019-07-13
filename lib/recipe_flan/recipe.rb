153657aad4860ebe86a67b2e514da6b75a562d9a
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-13 20:17:55 +0000

    modified recipe.rb fixed scraper_foodnetwork directions code

--- lib/recipe_flan/recipe.rb
+++ lib/recipe_flan/recipe.rb
@@ -22,9 +22,9 @@ attr_accessor :name, :ingredients, :directions, :reviews, :url
     doc = Nokogiri::HTML(open("https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016"))
     
     recipe = self.new  
-    recipe.name = doc.search("h1.o-AssetTitle__a-Headline").text.gsub("\n","").strip
-    recipe.ingredients = doc.search("div.o-Ingredients__m-Body").text.gsub("\n","").strip
-    recipe.directions = doc.search("div.o-Method__m-Step").text.gsub("\n","").strip
+    recipe.name = doc.search("h1.o-AssetTitle__a-Headline").text.strip
+    recipe.ingredients = doc.search("div.o-Ingredients__m-Body").text.strip
+    recipe.directions = doc.search("div.o-Method__m-Body").text.strip
     recipe.reviews = "73 reviews"
     recipe.url = "https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016"
     
@@ -35,10 +35,10 @@ attr_accessor :name, :ingredients, :directions, :reviews, :url
     doc = Nokogiri::HTML(open("https://www.allrecipes.com/recipe/20979/spanish-flan/"))
     
     recipe = self.new  
-    recipe.name = doc.search("h1.recipe-summary__h1").text.gsub("\n","").strip
-    recipe.ingredients = doc.search("li.checkList__line").text.gsub("\n","").strip
-    recipe.directions = doc.search("span.recipe-directions__list--item").text.gsub("\n","").strip
-    recipe.reviews = doc.search("span.review-count").text.gsub("\n","").strip
+    recipe.name = doc.search("h1.recipe-summary__h1").text.strip
+    recipe.ingredients = doc.search("li.checkList__line").text.strip
+    recipe.directions = doc.search("span.recipe-directions__list--item").text.strip
+    recipe.reviews = doc.search("span.review-count").text.strip
     recipe.url = "https://www.allrecipes.com/recipe/20979/spanish-flan/"
     
     recipe
