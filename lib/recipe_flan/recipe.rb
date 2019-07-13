df45015e0dd920ead12a60d8fb4ed23352e9b9f4
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-13 00:21:34 +0000

    modified recipe.rb

--- lib/recipe_flan/recipe.rb
+++ lib/recipe_flan/recipe.rb
@@ -21,26 +21,26 @@ attr_accessor :name, :ingredients, :directions, :reviews, :url
   def self.scrape_foodnetwork
     doc = Nokogiri::HTML(open("https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016"))
     
-    recipe_1 = self.new  
-    recipe_1.name = doc.search("h1.o-AssetTitle__a-Headline").text.gsub("\n","").strip
-    recipe_1.ingredients = doc.search("div.o-Ingredients__m-Body").text.gsub("\n","").strip
-    recipe_1.directions = doc.search("div.o-Method__m-Step").text.gsub("\n","").strip
-    recipe_1.reviews = "73 reviews"
-    recipe_1.url = "https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016"
+    recipe = self.new  
+    recipe.name = doc.search("h1.o-AssetTitle__a-Headline").text.gsub("\n","").strip
+    recipe.ingredients = doc.search("div.o-Ingredients__m-Body").text.gsub("\n","").strip
+    recipe.directions = doc.search("div.o-Method__m-Step").text.gsub("\n","").strip
+    recipe.reviews = "73 reviews"
+    recipe.url = "https://www.foodnetwork.com/recipes/tyler-florence/flan-recipe-1914016"
     
-    recipe_1
+    recipe
   end
   
   def self.scrape_allrecipes
     doc = Nokogiri::HTML(open("https://www.allrecipes.com/recipe/20979/spanish-flan/"))
     
-    recipe_2 = self.new  
-    recipe_2.name = doc.search("h1.recipe-summary__h1").text.gsub("\n","").strip
-    recipe_2.ingredients = doc.search("li.checkList__line").text.gsub("\n","").strip
-    recipe_2.directions = doc.search("span.recipe-directions__list--item").text.gsub("\n","").strip
-    recipe_2.reviews = doc.search("span.review-count").text.gsub("\n","").strip
-    recipe_2.url = "https://www.allrecipes.com/recipe/20979/spanish-flan/"
+    recipe = self.new  
+    recipe.name = doc.search("h1.recipe-summary__h1").text.gsub("\n","").strip
+    recipe.ingredients = doc.search("li.checkList__line").text.gsub("\n","").strip
+    recipe.directions = doc.search("span.recipe-directions__list--item").text.gsub("\n","").strip
+    recipe.reviews = doc.search("span.review-count").text.gsub("\n","").strip
+    recipe.url = "https://www.allrecipes.com/recipe/20979/spanish-flan/"
     
-    recipe_2
+    recipe
   end
 end
